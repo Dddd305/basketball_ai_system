253b1ae68fc2b67cd8bc36ab0ff1cdba11a00e4c
Dddd305 <kv23domushchi@gmail.com>
2026-04-25 17:48:29 +0300

    Connect to Neon cloud database

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Рядок підключення. Формат: postgresql://користувач:пароль@хост:порт/назва_бази
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:qwerty12345@localhost:5432/basketball_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://neondb_owner:npg_h0C9rlEwQFvn@ep-young-lake-alqfuwz1.c-3.eu-central-1.aws.neon.tech/neondb?sslmode=require"
 
 # Двигун, який фізично виконує запити до БД
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
