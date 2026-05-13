acbf7ba609e3a725b7e33b2c936ae1f46ae6b8dd
Dddd305 <kv23domushchi@gmail.com>
2026-05-14 01:19:06 +0300

    Update database.py

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 if not SQLALCHEMY_DATABASE_URL:
     raise ValueError("Помилка: DATABASE_URL не знайдено у файлі .env або системі!")
